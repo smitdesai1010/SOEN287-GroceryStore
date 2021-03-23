1deb54cc866d637e8d4a46060b4ff69bddd56500
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2021-03-23 07:43:00 -0400

    login done

--- Login/login.php
+++ Login/login.php
@@ -2,12 +2,28 @@
 
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $status = 1;
-
+    $status = 0;
+    $name;
     $xml = simplexml_load_file('../DataBase/user.xml');
 
-    echo var_dump($xml);
+    foreach ($xml->USER as $user)
+    {
+      
+      //because the string has extra characters. Spent 1 hour for this :(
+      $str = trim($user->EMAIL);    
 
-    // header("Location: /Login/login.html?status=$status");
-    // exit();
+      if ( $str == $email )
+      {
+          $status = 1;
+          $name = trim($user->NAME);
+
+          if ( trim($user->PASSWORD) == $password )
+            $status = 2;
+
+          break;
+      }  
+    }    
+    
+    header("Location: /Login/login.html?status=$status&name=$name");
+    exit();
 ?>
