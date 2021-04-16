07933c4006f12d180fbb287dda3f3ba957a6bea9
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2021-04-16 15:32:45 -0400

    fixed card size in specialprize

--- Aisles/SpecialProduct.php
+++ Aisles/SpecialProduct.php
@@ -3,13 +3,14 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <title>SOEN287</title>
 </head>
 
 
 <body>
-        <div>
             <h1 style="color: #FF0000; text-align: center;"> S  P  E  C  I  A  L  S </h1>
             
             <section class="px-4">
@@ -20,8 +21,11 @@
                         $row = "<div class='row d-flex justify-content-center'>";
 
                         echo $row;
-                            foreach($myfile->children() as $categoryName => $categoryProducts)
-                                foreach ($categoryProducts->PRODUCT as $prod) 
+
+                        foreach($myfile->children() as $categoryName => $categoryProducts)
+                            foreach ($categoryProducts->PRODUCT as $prod) 
+                            {
+                                if($prod->SPECIAL == 1) 
                                 {
                                     if ( $ctr!=0 && $ctr%3 == 0 )
                                     {
@@ -29,32 +33,32 @@
                                         echo $row;
                                     }
 
-                                    if($prod->SPECIAL == 1) 
-                                    {
-                                        echo "
-                                        <div class='column m-4' >
-                                            <div class='cardwidth card'>
-                                                <img class='crdimagesize card-img-top' src=$prod->THUMBNAIL alt='Card image cap'>
-                                                <div class='card-body'>
-                                                    <h5 class='card-title'>$prod->TITLE</h5>
-                                                    <span class='card-text d-flex justify-content-between'>
-                                                        <p style='color: red'><b> $$prod->SPECIALPRICE/lb </b></p>
-                                                        <input price=$prod->SPECIALPRICE placeholder='Quantity' id='demoInput' type='number' min='0' max='20' style='text-align: center;width: 40%; height: 75%'>            
-                                                    </span>
-                                                    <a id='add' style='background-color: #46B510 !important; border-color: #46B510 !important;' class='btn btn-primary'>Add to cart</a><!-- add reference to cart? --> 
-                                                    <a href='/Aisles/product.php?category=$categoryName&name=$prod->TITLE' style='background-color: #46B510 !important; border-color: #46B510 !important;' class='btn btn-primary'>More details</a>
-                                                </div>
+                                    echo "
+                                    <div class='column m-4' >
+                                        <div class='cardwidth card'>
+                                            <img class='crdimagesize card-img-top' src=$prod->THUMBNAIL alt='Card image cap'>
+                                            <div class='card-body'>
+                                                <h5 class='card-title'>$prod->TITLE</h5>
+                                                <span class='card-text d-flex justify-content-between'>
+                                                    <p style='color: red'><b> $$prod->SPECIALPRICE/lb </b></p>
+                                                    <input price=$prod->SPECIALPRICE placeholder='Quantity' id='demoInput' type='number' min='0' max='20' style='text-align: center;width: 40%; height: 75%'>            
+                                                </span>
+                                                <a id='add' style='background-color: #46B510 !important; border-color: #46B510 !important;' class='btn btn-primary'>Add to cart</a><!-- add reference to cart? --> 
+                                                <a href='/Aisles/product.php?category=$categoryName&name=$prod->TITLE' style='background-color: #46B510 !important; border-color: #46B510 !important;' class='btn btn-primary'>More details</a>
                                             </div>
-                                        </div>        
-                                        ";
-                                        ++$ctr;    
-                                    }
+                                        </div>
+                                    </div>        
+                                    ";
+                                    ++$ctr;    
                                 }
-                                echo "</div>"; 
+                            }
+                        
+                            echo "</div>"; 
                     ?>
                 </div>
             </section>
-        </div>
+
+
     <script src="../Navbar/navbar.js" abspath="../"></script>
     <script src="../Orders/aisle-order.js"></script>   
 </body>
